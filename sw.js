115b87afe5561828d750b2b8af56ad6ec78db38f
Kawin Glomjai <g.kawin@live.com>
2026-01-13 11:52:20 -0800

    Change service worker scope to '/exitpoll'

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
       const registration = await navigator.serviceWorker.register("/sw.js", {
-        scope: "/",
+        scope: "/exitpoll",
       });
       if (registration.installing) {
         console.log("Service worker installing");
